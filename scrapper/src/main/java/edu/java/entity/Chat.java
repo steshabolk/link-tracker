b1677fc5a40653d8cdfbce8e3d4328047d773f5f
steshabolk <steshabolk@yandex.ru>
2024-03-20 04:32:40 +0300

    scrapper: add jpa implementation for repository and service layers

--- scrapper/src/main/java/edu/java/entity/Chat.java
+++ scrapper/src/main/java/edu/java/entity/Chat.java
@@ -1,26 +1,80 @@
 package edu.java.entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import java.util.HashSet;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@EqualsAndHashCode(exclude = {"links"})
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
 @Setter
+@Entity
+@Table(name = "chats")
 public class Chat {
 
+    @Id
+    @SequenceGenerator(name = "chats_id_seq", sequenceName = "chats_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "chats_id_seq")
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "chat_id")
     private Long chatId;
 
     @Builder.Default
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinTable(name = "chats_links",
+               joinColumns = @JoinColumn(name = "chat_id"),
+               inverseJoinColumns = @JoinColumn(name = "link_id"))
     private Set<Link> links = new HashSet<>();
+
+    public void addLink(Link link) {
+        links.add(link);
+        link.getChats().add(this);
+    }
+
+    public void removeLink(Link link) {
+        links.remove(link);
+        link.getChats().remove(this);
+    }
+
+    public Optional<Link> findLinkByUrl(String url) {
+        return links.stream()
+            .filter(it -> it.getUrl().equals(url))
+            .findFirst();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Chat chat)) {
+            return false;
+        }
+        return id != null && Objects.equals(id, chat.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
--- scrapper/src/main/java/edu/java/entity/Link.java
+++ scrapper/src/main/java/edu/java/entity/Link.java
@@ -2,36 +2,74 @@ package edu.java.entity;
 
 import edu.java.enums.LinkStatus;
 import edu.java.enums.LinkType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import java.time.OffsetDateTime;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@EqualsAndHashCode(exclude = {"chats"})
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
 @Setter
+@Entity
+@Table(name = "links")
 public class Link {
 
+    @Id
+    @SequenceGenerator(name = "links_id_seq", sequenceName = "links_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "links_id_seq")
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "link_type")
+    @Enumerated(EnumType.ORDINAL)
     private LinkType linkType;
 
+    @Column(name = "url")
     private String url;
 
     @Builder.Default
+    @Column(name = "checked_at")
     private OffsetDateTime checkedAt = OffsetDateTime.now();
 
     @Builder.Default
+    @Column(name = "status")
+    @Enumerated(EnumType.ORDINAL)
     private LinkStatus status = LinkStatus.ACTIVE;
 
     @Builder.Default
+    @ManyToMany(mappedBy = "links")
     private Set<Chat> chats = new HashSet<>();
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Link link)) {
+            return false;
+        }
+        return id != null && Objects.equals(id, link.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
